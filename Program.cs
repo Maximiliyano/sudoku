cdca15051d9a71e3eabe729f1404687a3b2b78cc
Максим Василишин <maxim24152771@gmail.com>
2022-07-09 18:53:46 +0300

    Fix bugs with checker

--- Program.cs
+++ Program.cs
@@ -7,14 +7,12 @@ using System.Threading.Tasks;
 namespace validationSudoku
 {
     class Program
-    {
-        static readonly int N = 3;
-        
+    {   
         static bool ValidRow(int[,] matrix, int row)
         {
             for(int i = 0; i < matrix.GetLength(0); i++)
             {
-                if(matrix[row, i] < 0 || matrix[row, i] > N)
+                if(matrix[row,i] < 1 || matrix[row,i] > 9)
                 {
                     Console.WriteLine("Invalid value");
                     return false;
@@ -27,7 +25,7 @@ namespace validationSudoku
         {
             for(int i = 0; i < matrix.GetLength(1); i++)
             {
-                if(matrix[i, col] < 0 || matrix[i, col] > N)
+                if(matrix[i,col] < 1 || matrix[i,col] > 9)
                 {
                     Console.WriteLine("Invalid value");
                     return false;
@@ -42,7 +40,7 @@ namespace validationSudoku
             {
                 for(int col = 0; col < matrix.GetLength(1); col = col + 3)
                 {
-                    if(matrix[row, col] < 0 || matrix[row, col] > N)
+                    if(matrix[row,col] < 1 || matrix[row,col] > 9)
                     {
                         Console.WriteLine("Invalid value");
                         return false;
@@ -52,7 +50,7 @@ namespace validationSudoku
             return true;
         }
         
-        static void ValidCount(int[,] matrix) 
+        static void ValidCount(int[,] matrix, int N) 
         {
             for(int i = 0; i < N; i++)
             {   
@@ -74,14 +72,14 @@ namespace validationSudoku
             {
                 for(int col = 0; col < matrix.GetLength(1); col++)
                 {
-                    matrix[row, col] = random.Next(1, N);
+                    matrix[row,col] = random.Next(1, 9);
                 }
             }
         }
 
         static int[,] KeyBoardFillMatrix(int N)
         {
-            int[,] output = new int[N, N];
+            int[,] output = new int[N,N];
             for(int i = 0; i < N; i++) 
             {
                 string[] input = new string[N];
@@ -99,25 +97,59 @@ namespace validationSudoku
                 while (true);
                 for(int j = 0; j < N; j++)
                 {
-                    output[i, j] = int.Parse(input[j]);
+                    output[i,j] = int.Parse(input[j]);
                 }
             }
             return output;
         }
 
-        static void ValidSudoku(int[,] matrix) 
+        static int SumOfRow(int[,] matrix, int rowNum)
         {
-            int summa = 0, summas = 0;
-            for(int row = 0; row < matrix.GetLength(0); row++)
+            int sum = 0;
+            for(int i = 0; i < matrix.Length; i++)
             {
-                summa += matrix[row, 0];
-                for(int col = 0; col < matrix.GetLength(1); col++)
-                {
-                    summas += matrix[0, col];
-                }
+                sum += matrix[rowNum, i];
             }
-            if(summa != summas) Console.WriteLine("Sudoku is not valid!");
-            else Console.WriteLine("Sudoku is valid!");
+            return sum;
+        }
+
+        static int SumOfColumn(int[,] matrix, int columnNum)
+        {
+            int sum = 0;
+            for(int i = 0; i < matrix.Length; i++)
+            {
+                sum += matrix[i, columnNum];
+            }
+            return sum;
+        }
+
+        static int PivotSum(int N)
+        {
+            int sum = 0;
+            for (int i = 1; i <= N; i++)
+                sum += i;
+            return sum;
+        }
+
+        static bool IsCorrectSudocu(int[,] matrix)
+        {
+            int pivotSum = PivotSum(matrix.Length);
+
+            for(int i = 1; i < matrix.Length; i++)
+            {
+                int sumOfRow = SumOfRow(matrix, i);
+                if (sumOfRow != pivotSum)
+                    return false;
+            }
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                int sumOfСolumn = SumOfColumn(matrix, i);
+                if (sumOfСolumn != pivotSum)
+                    return false;
+            }
+
+            return true;
         }
 
         static void Display(int[,] matrix)
@@ -125,7 +157,7 @@ namespace validationSudoku
             for(int i = 0; i < matrix.GetLength(0); i++)
             {
                 for(int j = 0; j < matrix.GetLength(1); j++)
-                    Console.Write("\t" + matrix[i, j]);
+                    Console.Write("\t" + matrix[i,j]);
 
                 Console.WriteLine();
             }
@@ -133,19 +165,24 @@ namespace validationSudoku
 
         static void Main(string[] args)
         {
-            int[,] matrix = new int[N,N];
+            Console.Write("Enter size sudoku: ");
+            int input_size = Convert.ToInt16(Console.ReadLine());
+
+            if(input_size < 1) Console.Write("You entered wrong count!"); 
+
+            int[,] matrix = new int[input_size,input_size];
 
             Console.Write("Fill massive, enter 'true' (random) or 'false' (manual): ");
-            bool input_params = Convert.ToBoolean(Console.ReadLine());
+            bool input_statement = Convert.ToBoolean(Console.ReadLine());
 
-            if (!input_params) matrix = KeyBoardFillMatrix(N);
+            if (!input_statement) matrix = KeyBoardFillMatrix(input_size);
             else RandomFillMassive(matrix);
 
             // Валідація цифр
-            ValidCount(matrix);
+            ValidCount(matrix, input_size);
 
             // Валідація судоку
-            ValidSudoku(matrix);
+            //IsCorrectSudocu(matrix);
             
             // Вивід масиву
             Display(matrix);
